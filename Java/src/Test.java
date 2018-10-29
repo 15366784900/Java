d86ceef1eb2a078640ca150f0824f72a3ea228b3
15366784900 <2470634772@qq.com>
2018-10-29 16:48:55 +0800

    第二次提交

--- Java/src/Test.java
+++ Java/src/Test.java
@@ -4,6 +4,8 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("徐州再出发网络科技公司");
+		System.out.print("再出发网络科技公司");
+		
 	}
 
 }
